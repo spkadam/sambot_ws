5f6e9d9f98cf2bca95d9e0facdf016ba8982f4f6
skadam <skadam@danfoss.com>
2019-10-11 09:19:56 -0500

    launch files created

--- run_rviz.sh
+++ run_rviz.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-roslaunch sambot_description sambot_rviz.launch
-#roslaunch sambot_description sambot_rviz_amcl.launch 
+#roslaunch sambot_description sambot_rviz.launch
+roslaunch sambot_description sambot_rviz_amcl.launch 
